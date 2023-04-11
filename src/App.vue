8c9afff95d1e305bf27f561860e6de8f5485353d
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-04-11 12:28:45 +0200

    scaffolding

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,24 @@ export default {
 <template>
 
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea totam omnis quis, aut, consequuntur architecto autem cumque possimus error minus expedita in debitis! Veniam amet ducimus illum modi iure, eaque nisi totam recusandae consequatur! Consectetur, doloremque quisquam harum cumque et reiciendis tempora iste, sed consequuntur magni libero eos illum.</p>
+<div>
+<ul>
+  <li><img src="src/assets/svg/svg-0.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-1.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-2.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-3.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-4.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-5.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-6.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-7.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-8.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-9.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-10.svg" alt=""></li>
+  <li><img src="src/assets/svg/svg-11.svg" alt=""></li>
+</ul>
 
+
+</div>
 </template>
 
 <style lang="scss">
@@ -32,5 +49,9 @@ export default {
 
 p{
   color: $rosso;
+}
+img{
+  background-color: rebeccapurple;
+  height: 100px;
 }
 </style>
