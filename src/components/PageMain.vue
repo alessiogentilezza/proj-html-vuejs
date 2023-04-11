028dd7da7e4c5fa9d30d7a2ee81a40b238968f95
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-04-11 19:23:43 +0200

    seconda section ok

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="no-margin">
+  <section class="no-margin first">
 <ul class="d-flex">
   <li><img src="/h3-img-1.jpg" alt=""></li>
   <li><img src="/h3-img-2.jpg" alt=""></li>
@@ -7,6 +7,19 @@
   <li><img src="/h3-img-4.jpg" alt=""></li>
 </ul>    
   </section>
+
+  <section class="no-margin">
+    <div class="title">
+          <h3>"FORGET THE TRENDY PI77A SHOPS THIS HINDEN SPOT MAKES THE BEST NEW YORK-STYLE PIZZA SLICE IN NAPLES"</h3>
+
+    </div>
+
+<ul class="d-flex">
+  <li><img src="/h3-testimonials-bckgrnd.jpg" alt=""></li>
+</ul>
+
+  </section>
+
 
 
 
@@ -36,14 +49,25 @@ ul{
   margin: 10px 0;
 
 }
-  li{
+  .first li{
     width: calc(100% / 4);
     list-style: none;
     margin: 0 5px;
+  }
 
-    & img {
+  img {
     width:  100%;
   }
 
+  .title{
+    width: 50%;
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+
   }
+
 </style>
